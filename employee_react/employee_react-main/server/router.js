225556f5adefef9be9afee9cf405e9417544ee03
ANJITHA NAIR <117582345+24-ajta@users.noreply.github.com>
2023-12-05 15:59:11 +0530

    Add files via upload

---
+++ employee_react/employee_react-main/server/router.js
@@ -0,0 +1,32 @@
+import {Router} from "express";
+import auth from "./middlewares/auth.js"
+
+
+
+// import multer from "multer";
+import path from "path"
+
+
+ 
+import * as rh from "./requesthandler.js";
+
+// const storage = multer.diskStorage({
+//     destination:"./files",
+//     filename:(req,file,cb)=>{
+//         cb(null,file.originalname);
+// }})
+
+// const upload = multer({storage:storage});
+
+const router = Router()
+
+router.route("/register").post(rh.register);
+router.route("/login").post(rh.login);
+router.route("/profile/:id").get(rh.profile);
+router.route("/update/:id").put(rh.update);
+// router.route("/get-profile").get(auth,rh.getprofile);
+router.route("/get-file").get(rh.getfile);
+router.route("/listing").get(rh.listing);
+router.route("/deletedata/:id").delete(rh.deletedata)
+
+export default router;
---
+++ employee_react/employee_react-main/server/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./connection.js";
+import router from "./router.js"
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// app.post('/cors', (req, res) => {
+//     res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.send({ "msg": "This has CORS enabled " })
+// });
+app.use(cors({
+    orgin:'http://127.0.0.1:3000'
+}));
+
+app.use(express.json({
+    limit:"25mb"
+}));
+app.use(express.urlencoded({
+    extended:true,
+    limit:"25mb"
+}))
+
+
+
+
+app.use("/",express.static("./static"));
+app.use("/api",router);
+
+conn().then(()=>{
+    app.listen(process.env.PORT,error=>{
+        if(error){
+            console.log(error);
+            return;
+        }
+        return console.log("Server started on:   http://localhost:"+process.env.PORT); 
+    })
+})
+.catch(error=>{
+    console.log(error);
+})
