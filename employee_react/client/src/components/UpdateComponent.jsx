4243dff6bbfff89698efc064c2b92eba4efcdeb7
anjitha <nanjitha89@gmail.com>
2023-12-09 10:00:14 +0530

    formik validation

--- employee_react/client/src/components/UpdateComponent.jsx
+++ employee_react/client/src/components/UpdateComponent.jsx
@@ -651,15 +651,176 @@
 // export default UpdateComponent;
 
 
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import { useParams, Link } from "react-router-dom";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+// import axios from "axios";
+
+// function UpdateComponent() {
+//   const { id } = useParams();
+//   console.log("id",id)
+//   const [editData, seteditData] = useState({});
+
+//   useEffect(() => {
+//     getDetails();
+//   }, []);
+
+//   const getDetails = async () => {
+//     try {
+//       const response = await axios.get(`http://localhost:3000/api/profile/${id}`);
+//       seteditData(response.data.data);
+//     } catch (error) {
+//       console.error("Error fetching user details:", error);
+//     }
+//   };
+
+//   const handleSubmit = async (values,{resetForm}) => {
+//     try {
+//       const response = await axios.put(`http://localhost:3000/api/update/${id}`, values);
+//       console.log("Form Submitted", response.data.data);
+//       resetForm();
+//       // You might set some state or perform further actions upon successful submission
+//     } catch (error) {
+//       console.error("Error submitting form:", error);
+//     }
+//   };
+
+//   const SignupSchema = Yup.object().shape({
+//     name: Yup.string()
+//     .min(2, "Too Short!")
+//     .max(50, "Too Long!")
+//     .required("Required"),
+
+//   email: Yup.string().email("Invalid email").required("Required"),
+
+//   place: Yup.string().min(2, "Invalid Address").required("Required"),
+
+//   designation: Yup.string().required("Required"),
+
+//   password: Yup.string()
+//     .required("No password provided.")
+//     .min(8, "Password is too short - should be 8 chars minimum.")
+//     .matches(
+//       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+//       "Password must contain one Uppercase and one lowercase and a number."
+//     ),
+
+//   contact: Yup.string().matches(
+//     /^[6-9]\d{9}$/,
+//     "Please enter valid phone number."
+//   ),
+//   });
+  // const onDelete = async () => {
+  //   axios
+  //     .delete(`http://localhost:3000/api/deletedata/${id}`)
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+//   return (
+//     <>
+//       <h1 style={{ textAlign: "center" }}>Details </h1>
+//       <div className="container">
+//         <Formik
+//           initialValues={{
+//             name: editData.name || '',
+//             email: editData.email || '',
+//             place: editData.place || '',
+//             designation: editData.designation || '',
+//             contact: editData.contact || '',
+//           }}
+//           onSubmit={handleSubmit}
+//           validationSchema={SignupSchema}
+//           enableReinitialize
+//         >
+//             <Form className="mt-5">
+//               {/* Form fields */}
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <label htmlFor="name" style={{ color: "blue" }}>
+//                   Name
+//                   <Field
+//                     className="form-control border border-primary"
+//                     type="text"
+//                     name="name"
+//                   />
+//                   <ErrorMessage name="name" style={{ color: "red" }} component="div" />
+//                 </label>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <label htmlFor="email" style={{ color: "blue" }}>
+//                   Email
+//                   <Field
+//                     className="form-control border border-primary"
+//                     type="email"
+//                     name="email"
+//                   />
+//                   <ErrorMessage name="email" style={{ color: "red" }} component="div" />
+//                 </label>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <label htmlFor="place" style={{ color: "blue" }}>
+//                   Place
+//                   <Field
+//                     className="form-control border border-primary"
+//                     type="text"
+//                     name="place"
+//                   />
+//                   <ErrorMessage name="place" style={{ color: "red" }} component="div" />
+//                 </label>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <label htmlFor="designation" style={{ color: "blue" }}>
+//                   Type of Work
+//                   <Field
+//                     className="form-control border border-primary"
+//                     type="text"
+//                     name="designation"
+//                   />
+//                   <ErrorMessage name="designation" style={{ color: "red" }} component="div" />
+//                 </label>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <label htmlFor="contact" style={{ color: "blue" }}>
+//                   Name
+//                   <Field
+//                     className="form-control border border-primary"
+//                     type="text"
+//                     name="contact"
+//                   />
+//                   <ErrorMessage name="contact" style={{ color: "red" }} component="div" />
+//                 </label>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <button type="submit" className="btn btn-primary" style={{ color: "white" }}>
+//                   Update Details
+//                 </button>
+//               </div>
+//               <div className="form-group text-center" style={{ padding: "20px" }}>
+//                 <Link to="/view">
+//                   <button className="btn btn-primary" onClick={onDelete}>
+//                     Delete
+//                   </button>
+//                 </Link>
+//               </div>
+//             </Form>
+//         </Formik>
+//       </div>
+//     </>
+//   );
+// }
+// export default UpdateComponent;
+
+import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 function UpdateComponent() {
   const { id } = useParams("");
-  const [editData, seteditData] = useState({});
+  console.log("id",id)
+  const [editData, setEditData] = useState({});
 
   useEffect(() => {
     getDetails();
@@ -668,46 +829,39 @@ function UpdateComponent() {
   const getDetails = async () => {
     try {
       const response = await axios.get(`http://localhost:3000/api/profile/${id}`);
-      seteditData(response.data.data);
+      formik.setValues(response.data.data)
+      setEditData(response.data.data);
     } catch (error) {
       console.error("Error fetching user details:", error);
     }
   };
 
-  const handleSubmit = async (values) => {
-    try {
-      const response = await axios.put(`http://localhost:3000/api/update/${id}`, values);
-      console.log("Form Submitted", response.data.data);
-      // You might set some state or perform further actions upon successful submission
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    place: Yup.string().min(2, "Invalid Address").required("Required"),
+    designation: Yup.string().required("Required"),
+    contact: Yup.string().matches(/^[6-9]\d{9}$/, "Please enter a valid phone number.").required("Required"),
+  });
 
-  const SignupSchema = Yup.object().shape({
-    name: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-
-  email: Yup.string().email("Invalid email").required("Required"),
-
-  place: Yup.string().min(2, "Invalid Address").required("Required"),
-
-  designation: Yup.string().required("Required"),
-
-  password: Yup.string()
-    .required("No password provided.")
-    .min(8, "Password is too short - should be 8 chars minimum.")
-    .matches(
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
-      "Password must contain one Uppercase and one lowercase and a number."
-    ),
-
-  contact: Yup.string().matches(
-    /^[6-9]\d{9}$/,
-    "Please enter valid phone number."
-  ),
+  const formik = useFormik({
+    initialValues: {
+      name: editData.name || '',
+      email: editData.email || '',
+      place: editData.place || '',
+      designation: editData.designation || '',
+      contact: editData.contact || '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.put(`http://localhost:3000/api/update/${id}`, values);
+        console.log("Form Submitted", response.data.data);
+        // You might set some state or perform further actions upon successful submission
+      } catch (error) {
+        console.error("Error submitting form:", error);
+      }
+    },
   });
   const onDelete = async () => {
     axios
@@ -716,97 +870,101 @@ function UpdateComponent() {
         setData(response.data);
       });
   };
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Details </h1>
+      <h1 style={{ textAlign: "center" }}>Details</h1>
       <div className="container">
-        <Formik
-          initialValues={{
-            name: editData.name || '',
-            email: editData.email || '',
-            place: editData.place || '',
-            designation: editData.designation || '',
-            contact: editData.contact || '',
-          }}
-          onSubmit={handleSubmit}
-          validationSchema={SignupSchema}
-          enableReinitialize
-        >
-          {({ errors, touched }) => (
-            <Form className="mt-5">
-              {/* Form fields */}
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <label htmlFor="name" style={{ color: "blue" }}>
-                  Name
-                  <Field
-                    className="form-control border border-primary"
-                    type="text"
-                    name="name"
-                  />
-                  <ErrorMessage name="name" style={{ color: "red" }} component="div" />
-                </label>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <label htmlFor="email" style={{ color: "blue" }}>
-                  Email
-                  <Field
-                    className="form-control border border-primary"
-                    type="email"
-                    name="email"
-                  />
-                  <ErrorMessage name="email" style={{ color: "red" }} component="div" />
-                </label>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <label htmlFor="place" style={{ color: "blue" }}>
-                  Place
-                  <Field
-                    className="form-control border border-primary"
-                    type="text"
-                    name="place"
-                  />
-                  <ErrorMessage name="place" style={{ color: "red" }} component="div" />
-                </label>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <label htmlFor="designation" style={{ color: "blue" }}>
-                  Type of Work
-                  <Field
-                    className="form-control border border-primary"
-                    type="text"
-                    name="designation"
-                  />
-                  <ErrorMessage name="designation" style={{ color: "red" }} component="div" />
-                </label>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <label htmlFor="contact" style={{ color: "blue" }}>
-                  Name
-                  <Field
-                    className="form-control border border-primary"
-                    type="text"
-                    name="contact"
-                  />
-                  <ErrorMessage name="contact" style={{ color: "red" }} component="div" />
-                </label>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <button type="submit" className="btn btn-primary" style={{ color: "white" }}>
-                  Update Details
-                </button>
-              </div>
-              <div className="form-group text-center" style={{ padding: "20px" }}>
-                <Link to="/view">
-                  <button className="btn btn-primary" onClick={onDelete}>
-                    Delete
-                  </button>
-                </Link>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        <form onSubmit={formik.handleSubmit} className="mt-5">
+          {/* Form fields */}
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <label htmlFor="name" style={{ color: "blue" }}>
+              Name
+              <input
+                className="form-control border border-primary"
+                type="text"
+                name="name"
+                {...formik.getFieldProps("name")}
+              />
+              {formik.touched.name && formik.errors.name && (
+                <div style={{ color: "red" }}>{formik.errors.name}</div>
+              )}
+            </label>
+          </div>
+
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <label htmlFor="email" style={{ color: "blue" }}>
+              Email
+              <input
+                className="form-control border border-primary"
+                type="email"
+                name="email"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              )}
+            </label>
+          </div>
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <label htmlFor="place" style={{ color: "blue" }}>
+              Place
+              <input
+                className="form-control border border-primary"
+                type="text"
+                name="place"
+                {...formik.getFieldProps("place")}
+              />
+              {formik.touched.place && formik.errors.place && (
+                <div style={{ color: "red" }}>{formik.errors.place}</div>
+              )}
+            </label>
+          </div>
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <label htmlFor="designation" style={{ color: "blue" }}>
+              Work
+              <input
+                className="form-control border border-primary"
+                type="text"
+                name="designation"
+                {...formik.getFieldProps("designation")}
+              />
+              {formik.touched.designation && formik.errors.designation && (
+                <div style={{ color: "red" }}>{formik.errors.designation}</div>
+              )}
+            </label>
+          </div>
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <label htmlFor="contact" style={{ color: "blue" }}>
+              Contact
+              <input
+                className="form-control border border-primary"
+                type="text"
+                name="contact"
+                {...formik.getFieldProps("contact")}
+              />
+              {formik.touched.contact && formik.errors.contact && (
+                <div style={{ color: "red" }}>{formik.errors.contact}</div>
+              )}
+            </label>
+          </div>
+          {/* ... Other form fields */}
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <button type="submit" className="btn btn-primary" style={{ color: "white" }}>
+              Update Details
+            </button>
+          </div>
+          <div className="form-group text-center" style={{ padding: "20px" }}>
+            <Link to="/view">
+              <button className="btn btn-primary" onClick={onDelete}>
+                Delete
+              </button>
+            </Link>
+          </div>
+        </form>
       </div>
     </>
   );
 }
+
 export default UpdateComponent;
